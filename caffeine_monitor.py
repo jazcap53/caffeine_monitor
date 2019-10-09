654f3153d935e5c2c91864c056ff7af77d9c15e1
Andrew Jarcho <andrew.jarcho@gmail.com>
2019-10-09 08:58:17 -0400

    Make monitor write to file
    On branch master
    Changes to be committed:
        modified:   .gitignore
        modified:   caffeine_monitor.py

--- caffeine_monitor.py
+++ caffeine_monitor.py
@@ -9,28 +9,33 @@
 # 3. add a new amount (now or at some previous time)
 
 from datetime import datetime, timedelta
-import math
+# import math
 
 
 class CoffeeMonitor:
     half_life = 360  # in minutes
 
-    def __init__(self, infile):
-        self.infile = infile
+    def __init__(self, iofile):
+        self.iofile = iofile
         self.old_time = None
         self.level = None
         self.curr_time = None
 
     def read_file(self):
         try:
-            old_time_str = infile.readline().strip()
+            old_time_str = self.iofile.readline().strip().lstrip('\x00')
             self.old_time = datetime.strptime(old_time_str, '%Y-%m-%d_%H:%M')
-            level_str = infile.readline().strip()
+            level_str = self.iofile.readline().strip()
             self.level = float(level_str)
         except FileNotFoundError:
             self.old_time = datetime.today()
             self.level = 0.0
-    
+
+    def write_file(self):
+        self.iofile.truncate(0)
+        print(datetime.strftime(self.curr_time, '%Y-%m-%d_%H:%M'), file=self.iofile)
+        print(self.level, file=self.iofile)
+
     def decay(self):
         self.curr_time = datetime.today()
         minutes_elapsed = (self.curr_time - self.old_time) / timedelta(minutes=1)
@@ -47,13 +52,12 @@ class CoffeeMonitor:
 
 
 if __name__ == '__main__':
-    with open('caffeine.txt', 'r') as infile:
-        monitor = CoffeeMonitor(infile)
+    with open('caffeine.txt', 'r+') as storage:
+        monitor = CoffeeMonitor(storage)
         monitor.read_file()
         print(monitor)
         monitor.decay()
         print(monitor)
         monitor.add_caffeine(16)
         print(monitor)
-
-
+        monitor.write_file()
