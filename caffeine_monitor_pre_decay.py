c250436804040ed827f0daeab0d22b4b54699ad0
Andrew Jarcho <andrew.jarcho@gmail.com>
2019-10-21 09:34:24 -0400

    Add test version 'caffeine_monitor_pre_decay.py' to repository
    The test version takes an optional second argument -- how long ago the amount
    from the first argument was consumed.
    
    On branch pre_decay
    Changes to be committed:
        new file:   caffeine_monitor_pre_decay.py

---
+++ caffeine_monitor_pre_decay.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3.7
+
+# file: caffeine_monitor.py
+# andrew jarcho
+# 2019-10-08
+
+# Answer question: how many mg of caffeine are in me now?
+
+
+from datetime import datetime, timedelta
+import sys
+import json
+from pathlib import Path
+import logging
+
+
+logging.basicConfig(filename='caffeine_pre_decay.log', level=logging.INFO,
+                    format='%(message)s')
+
+
+class CoffeeMonitor:
+    half_life = 360  # in minutes
+
+    def __init__(self, iofile, mg=0, pre_decay_mins=0):
+        self.iofile = iofile
+        self.old_time = None
+        self.level = None
+        self.curr_time = None
+        self.data_dict = {}
+        self.mg_to_add = mg
+        self.pre_decay_mins = pre_decay_mins
+
+    def main(self):
+        self.read_file()
+        self.decay()
+        if self.pre_decay_mins:
+            self.pre_decay()
+        if self.mg_to_add:
+            self.add_caffeine()
+        self.update_time()
+        self.write_file()
+        print(self)
+
+    def read_file(self):
+        self.data_dict = json.load(self.iofile)
+        self.old_time = datetime.strptime(self.data_dict['time'],
+                                          '%Y-%m-%d_%H:%M')
+        level_str = self.data_dict['level']
+        self.level = float(level_str)
+
+    def write_file(self):
+        self.iofile.seek(0)
+        self.iofile.truncate(0)
+        log_mesg = f'level is {round(self.data_dict["level"], 1)} at {self.data_dict["time"]}'
+        if self.mg_to_add:
+            log_mesg = f'{self.mg_to_add} mg added: ' + log_mesg
+            logging.info(log_mesg)
+        else:
+            logging.debug(log_mesg)
+        json.dump(self.data_dict, self.iofile)
+
+    def decay(self):
+        self.curr_time = datetime.today()
+        minutes_elapsed = (self.curr_time -
+                           self.old_time) / timedelta(minutes=1)
+        self.data_dict['time'] = datetime.strftime(self.curr_time,
+                                                   '%Y-%m-%d_%H:%M')
+        self.level *= pow(0.5, (minutes_elapsed / self.half_life))
+        self.data_dict['level'] = self.level
+
+    def pre_decay(self):
+        curr_time = datetime.today()
+        old_time = curr_time - timedelta(minutes=self.pre_decay_mins)
+        minutes_elapsed = (curr_time - old_time) / timedelta(minutes=1)
+        self.mg_to_add *= pow(0.5, (minutes_elapsed / self.half_life))
+
+    def add_caffeine(self):
+        self.level += self.mg_to_add
+        self.data_dict['level'] += self.mg_to_add
+
+    def update_time(self):
+        self.data_dict['time'] = datetime.strftime(datetime.today(),
+                                                   '%Y-%m-%d_%H:%M')
+
+    def __str__(self):
+        return (f'Caffeine level is {round(self.level, 1)} mg at time '
+                f'{self.data_dict["time"]}')
+
+
+def init_storage(fname):
+    outfile = open(fname, 'w')
+    time_now = datetime.strftime(datetime.today(), '%Y-%m-%d_%H:%M')
+    start_level = 0
+    json.dump({"time": time_now, "level": start_level}, outfile)
+    outfile.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 3:
+        print('Usage: program_name <mgs_of_caffeine_to_add> <pre-decay in minutes>')
+        sys.exit(0)
+
+    filename = 'caffeine_pre_decay.json'
+    my_file = Path(filename)
+    if not my_file.is_file():
+        init_storage(filename)
+    with open(filename, 'r+') as storage:
+        monitor = CoffeeMonitor(storage,
+                                int(sys.argv[1]) if len(sys.argv) > 1 else 0,
+                                int(sys.argv[2]) if len(sys.argv) > 2 else 0)
+        monitor.main()
