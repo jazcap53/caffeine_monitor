5b78da59d8c4b49b9881921c3f60f68d35a0ee0f
Andrew Jarcho <andrew.jarcho@gmail.com>
2020-03-27 18:28:49 -0400

    Put tests into their own subdirectory; put all non-class functions together
    On branch rearrange_code
    Changes to be committed:
        modified:   caffeine_monitor.py
        renamed:    conftest.py -> tests/conftest.py
        renamed:    test_caffeine_monitor.py -> tests/test_caffeine_monitor.py

--- caffeine_monitor.py
+++ caffeine_monitor.py
@@ -19,52 +19,6 @@ import configparser
 import argparse
 
 
-def check_which_environment():
-    try:
-        which_environment = os.environ['CAFF_ENV']
-        if which_environment not in ('prod', 'test'):
-            raise KeyError
-    except KeyError:
-        print('\nPlease export environment variable CAFF_ENV as '
-              'prod or test\n')
-        sys.exit(0)
-    return which_environment
-
-
-def read_config_file(config_file):
-    conf = configparser.ConfigParser()
-    # config.read('caffeine.ini')
-    conf.read(config_file)
-    return conf
-
-
-def parse_clas():
-    parser = argparse.ArgumentParser(description='Estimate the quantity '
-                                                 'of caffeine (in mg) in the '
-                                                 'user\'s body')
-    parser.add_argument('-t', '--test', action='store_true',
-                        help='Use test environment')
-    parser.add_argument('mg', nargs='?', type=int, default=0,
-                        help='mg of caffeine to add (may be negative, 0, or '
-                             'omitted)')
-    parser.add_argument('mins', nargs='?', type=int, default=0,
-                        help='minutes ago caffeine was added (may be negative, '
-                        '0, or omitted)')
-    return parser.parse_args()
-
-
-def check_cla_match_env(cur_env, args):
-    args.test = True if '-t' in sys.argv or '--test' in sys.argv else False
-
-    if args.test and cur_env == 'prod':
-        print("Please switch to the test environment with 'export CAFF_ENV=test'")
-        sys.exit(0)
-
-    if not args.test and cur_env == 'test':
-        print("You may switch to the production environment with 'export CAFF_ENV=prod'")
-        sys.exit(0)
-
-
 class CaffeineMonitor:
     half_life = 360  # in minutes
 
@@ -137,6 +91,51 @@ class CaffeineMonitor:
     def __str__(self):
         return (f'Caffeine level is {round(self.data_dict["level"], 1)} '
                 f'mg at time {self.data_dict["time"]}')
+
+
+def check_which_environment():
+    try:
+        which_environment = os.environ['CAFF_ENV']
+        if which_environment not in ('prod', 'test'):
+            raise KeyError
+    except KeyError:
+        print('\nPlease export environment variable CAFF_ENV as '
+              'prod or test\n')
+        sys.exit(0)
+    return which_environment
+
+
+def read_config_file(config_file):
+    conf = configparser.ConfigParser()
+    conf.read(config_file)
+    return conf
+
+
+def parse_clas():
+    parser = argparse.ArgumentParser(description='Estimate the quantity '
+                                                 'of caffeine (in mg) in the '
+                                                 'user\'s body')
+    parser.add_argument('-t', '--test', action='store_true',
+                        help='Use test environment')
+    parser.add_argument('mg', nargs='?', type=int, default=0,
+                        help='mg of caffeine to add (may be negative, 0, or '
+                             'omitted)')
+    parser.add_argument('mins', nargs='?', type=int, default=0,
+                        help='minutes ago caffeine was added (may be negative, '
+                        '0, or omitted)')
+    return parser.parse_args()
+
+
+def check_cla_match_env(cur_env, args):
+    args.test = True if '-t' in sys.argv or '--test' in sys.argv else False
+
+    if args.test and cur_env == 'prod':
+        print("Please switch to the test environment with 'export CAFF_ENV=test'")
+        sys.exit(0)
+
+    if not args.test and cur_env == 'test':
+        print("You may switch to the production environment with 'export CAFF_ENV=prod'")
+        sys.exit(0)
 
 
 def init_storage(fname):
