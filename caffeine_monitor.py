275bb70c7bf3b08b18ba34ef316797691f95af9c
Andrew Jarcho <andrew.jarcho@gmail.com>
2020-03-27 09:39:23 -0400

    Edit some of text output by arg parser
    On branch write_tests_w_tdd
    Changes to be committed:
        modified:   caffeine_monitor.py

--- caffeine_monitor.py
+++ caffeine_monitor.py
@@ -38,13 +38,17 @@ def read_config_file(config_file):
 
 
 def parse_clas():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='Estimate the quantity '
+                                                 'of caffeine (in mg) in the '
+                                                 'user\'s body')
     parser.add_argument('-t', '--test', action='store_true',
                         help='Use test environment')
     parser.add_argument('mg', nargs='?', type=int, default=0,
-                        help='mg of caffeine to add (may be negative)')
+                        help='mg of caffeine to add (may be negative, 0, or '
+                             'omitted)')
     parser.add_argument('mins', nargs='?', type=int, default=0,
-                        help='minutes ago caffeine was added (may be negative)')
+                        help='minutes ago caffeine was added (may be negative, '
+                        '0, or omitted)')
     return parser.parse_args()
 
 
@@ -158,8 +162,8 @@ def init_storage(fname):
 
 if __name__ == '__main__':
     if len(sys.argv) > 4:
-        print('Usage: program_name <mgs of caffeine to add> '
-              '<minutes ago caffeine was added>')
+        print('Usage: program_name [mgs of caffeine to add] '
+              '[minutes ago caffeine was added] [-t | --test flag]')
         sys.exit(0)
 
     filename = config[current_environment]['json_file']
