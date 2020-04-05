ddc3f2c3197e7ba4bf400062621f89d88f644225
Andrew Jarcho <andrew.jarcho@gmail.com>
2020-04-05 15:05:45 -0400

    Remove commented-out code
    On branch make_utils_file
    Changes to be committed:
        modified:   src/caffeine_monitor.py

--- src/caffeine_monitor.py
+++ src/caffeine_monitor.py
@@ -105,45 +105,10 @@ class CaffeineMonitor:
                 f'mg at time {self.data_dict["time"]}')
 
 
-# def check_which_environment():
-#     """
-#     :return: the current environment ('prod' or 'test')
-#     """
-#     try:
-#         which_environment = os.environ['CAFF_ENV']
-#         if which_environment not in ('prod', 'test'):
-#             raise KeyError
-#     except KeyError:
-#         print('\nPlease export environment variable CAFF_ENV as '
-#               'prod or test\n')
-#         sys.exit(0)
-#     return which_environment
-
-
 def read_config_file(config_file):
     conf = configparser.ConfigParser()
     conf.read(config_file)
     return conf
-
-
-# def parse_clas():
-#     """
-#     Parse the command line arguments
-#     :return: an argparse.Namespace instance
-#     """
-#     parser = argparse.ArgumentParser(description='Estimate the quantity '
-#                                                  'of caffeine (in mg) in the '
-#                                                  'user\'s body')
-#     parser.add_argument('-t', '--test', action='store_true',
-#                         help='Use test environment')
-#     parser.add_argument('mg', nargs='?', type=int, default=0,
-#                         help='mg of caffeine to add (may be negative, 0, or '
-#                              'omitted)')
-#     parser.add_argument('mins', nargs='?', type=int, default=0,
-#                         help='minutes ago caffeine was added '
-#                              '(may be negative, 0, or '
-#                              'omitted)')
-#     return parser.parse_args()
 
 
 def check_cla_match_env(cur_env, ags):
